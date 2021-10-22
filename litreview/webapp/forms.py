b13a57ea5833f857eef7eba0a214720aa6c0a945
tom gauthier <tomgauthier0@gmail.com>
2021-10-22 09:42:41 +0200

    implementing User

--- litreview/webapp/forms.py
+++ litreview/webapp/forms.py
@@ -5,4 +5,4 @@ from webapp.models import Ticket
 class TicketForm(ModelForm):
     class Meta:
         model = Ticket
-        fields = ['title', 'description', 'image']
+        fields = ['title', 'description', 'image', 'user']
